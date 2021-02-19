09557a3ba8d8bc9f649bc3a5a2a6b4ad29c0ae42
seff0 <76668281+seff0@users.noreply.github.com>
2021-02-18 19:46:20 -0700

    flesh out inquirer functions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
-function createEmployee() {
+function createManager() {
   inquirer
     .prompt([
       {
@@ -24,7 +24,7 @@ function createEmployee() {
       {
         type: "input",
         message: "Enter the office number.",
-        name: "managerName",
+        name: "managerOffice",
       },
       {
         type: "list",
@@ -43,3 +43,87 @@ function createEmployee() {
       }
     });
 }
+
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "engineerName",
+      },
+      {
+        type: "input",
+        message: "Enter the engineer's employee ID.",
+        name: "engineerID",
+      },
+      {
+        type: "input",
+        message: "Enter the engineer's email address.",
+        name: "engineerEmail",
+      },
+      {
+        type: "input",
+        message: "Enter the engineer's GitHub username.",
+        name: "engineerGitHub",
+      },
+      {
+        type: "list",
+        choices: ["Engineer", "Intern", "I am done adding team members."],
+        message: "Would you like to add another team member?",
+        name: "moreMembers",
+      },
+    ])
+    .then((response) => {
+      if (response.moreMembers === "Engineer") {
+        createEngineer();
+      } else if (response.moreMembers === "Intern") {
+        createIntern();
+      } else {
+        createHTML();
+      }
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "internName",
+      },
+      {
+        type: "input",
+        message: "Enter the intern's employee ID.",
+        name: "internID",
+      },
+      {
+        type: "input",
+        message: "Enter the intern's email address.",
+        name: "internEmail",
+      },
+      {
+        type: "input",
+        message: "Enter the intern's school.",
+        name: "internSchool",
+      },
+      {
+        type: "list",
+        choices: ["Engineer", "Intern", "I am done adding team members."],
+        message: "Would you like to add another team member?",
+        name: "moreMembers",
+      },
+    ])
+    .then((response) => {
+      if (response.moreMembers === "Engineer") {
+        createEngineer();
+      } else if (response.moreMembers === "Intern") {
+        createIntern();
+      } else {
+        createHTML();
+      }
+    });
+}
+
+createManager();
